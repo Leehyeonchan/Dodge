f312fc473cc5f877ad2e3f9b13bde1c082bca360
이현찬 <guscksdl3460@gmail.com>
2022-02-23 17:28:09 +0900

    20220223_hyeonchan

---
+++ Dodge/Assets/playerControler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+    //이동에 사용될 리지드바디 컴포넌트
+    // public Rigidbody playerRigidbody;
+    // 이동 출력
+    public float speed = 8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

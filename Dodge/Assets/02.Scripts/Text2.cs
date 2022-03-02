4a6dac215c4b9945e4ad3c15e51822b4dbcd442c
tladud123 <guscksdl3460@gmail.com>
2022-03-02 16:25:09 +0900

    20220302_last

---
+++ Dodge/Assets/02.Scripts/Text2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text2 : MonoBehaviour
+{
+    // 한글 인코딩 테스트1\
+    // 한글 인코딩 테스트2
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
